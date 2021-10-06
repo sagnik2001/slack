eaf7d8699da50f72d7efa28b0bca95a7a216fdb1
Sagnikpal <Palsagnik2001@gmail.com>
2021-10-06 22:16:50 +0530

    Update the auth change

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
-import {React,useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import "semantic-ui-css/semantic.min.css";/*So That we can use it everywhere*/
 import App from './App';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import LogIn from './Components/LogIn/LogIn.js';
-import Register from './Components/Register/Register.js';
-import { withRouter } from 'react-router-dom'
 
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
+
+import Register from "./Components/Register/Register";
+import LogIn from "./Components/LogIn/LogIn";
+import firebase from "./Components/database/Firebase";
+
+
+import "semantic-ui-css/semantic.min.css"
+
+
+const Index = (props) => {
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+
+        props.history.push("/");
+      } else {
+
+        props.history.push("/login");
+      }
+    })
+  }, []);
+
+
+  return (
+    <Switch>
+      <Route path="/login" component={LogIn} />
+      <Route path="/register" component={Register} />
+      <Route path="/" component={App} />
+    </Switch>)
+}
+
+
+
+const IndexWithRouter = withRouter(Index);
 
 ReactDOM.render(
   <React.StrictMode>
-//     <Router>
-//       <Switch>
-// /*         <Route path="/login" component={LogIn}/>
-//          <Route path="/register" component={Register}/>*/
-//          <Route path="/" component={App}/>
-//       </Switch>
-//     </Router>
-<App/>
 
-</React.StrictMode>,
+      <Router>
+        <IndexWithRouter />
+      </Router>
+
+  </React.StrictMode>,
   document.getElementById('root')
 );
